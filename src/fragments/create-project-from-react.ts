1f15ad3ec8b3918e174683e5955f6a61356ab8c4
Roy Sommer <roy@sommer.co.il>
2018-07-15 02:45:59 +0300

    react recipe readiness

--- src/fragments/create-project-from-react.ts
+++ src/fragments/create-project-from-react.ts
@@ -6,12 +6,46 @@ import { appendToFile, mv, writeFile } from '../utils/fs';
 import { moduleDeclaration, replaceImport } from '../actions/typescript';
 
 const configOverridesTemplate =
-  `var rewireSass = require('react-app-rewire-scss');
+  `const rewireSass = require('react-app-rewire-scss');
+const { getLoader } = require('react-app-rewired');
+
+function rewireCssModules(config, env) {
+  const cssRules = getLoader(config.module.rules, rule => String(rule.test) === String(/\\.css$/));
+  const cssLoaderConfig = (cssRules.use || cssRules.loader || []).find(x => x.loader === require.resolve('css-loader'))
+  if (cssLoaderConfig) {
+  	cssLoaderConfig.options = { ...(cssLoaderConfig.options || {}), modules: true, camelCase: true};
+  }
+  return config;
+}
 
 module.exports = function override(config, env) {
+  config = rewireCssModules(config, env);
   config = rewireSass(config, env);
   return config;
 }
+`;
+
+const appTsxTemplate =
+  `import * as React from 'react';
+import * as style from './App.scss';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className={style.app}>
+        <header className={style.appHeader}>
+          <img src={require('./logo.svg')} className={style.appLogo} alt="logo" />
+          <h1 className={style.appTitle}>Welcome to React</h1>
+        </header>
+        <p className={style.appIntro}>
+          To get started, edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
 `;
 
 export const createProjectWithReact = async () => {
@@ -27,9 +61,9 @@ export const createProjectWithReact = async () => {
   logger.pending('renaming files');
   await mv('src/App.css', 'src/App.scss');
   await mv('src/index.css', 'src/index.scss');
-  logger.pending('replacing imports');
-  await replaceImport('src/App.tsx', './App.css', './App.scss');
+  logger.pending('altering tsx files');
   await replaceImport('src/index.tsx', './index.css', './index.scss');
+  await writeFile('src/App.tsx', appTsxTemplate);
   logger.pending('adding declarations');
   await mv('images.d.ts', 'src/external-types.d.ts');
   await appendToFile('src/external-types.d.ts', moduleDeclaration('*.scss'));
@@ -43,7 +77,7 @@ export const createProjectWithReact = async () => {
   await addScript('start', 'react-app-rewired start --scripts-version react-scripts-ts');
   await addScript('build', 'react-app-rewired build --scripts-version react-scripts-ts');
   await addScript('test', 'react-app-rewired test --scripts-version react-scripts-ts --env=jsdom');
-  logger.pending('create config overrides');
+  logger.pending('creating config overrides');
   await writeFile('config-overrides.js', configOverridesTemplate);
   logger.success();
 };
