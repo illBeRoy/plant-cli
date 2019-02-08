debe503e199b469ff870dc5e0777173c114db4f7
Roy Sommer <roy@sommer.co.il>
2019-02-09 01:12:00 +0200

    fix: react-native recipe now works

--- src/actions/expo.ts
+++ src/actions/expo.ts
@@ -2,5 +2,5 @@ import { installThirdPartyTool, useThirdPartyTool } from '../utils/3rd-party';
 
 export const expo = async (args = '') => {
   await installThirdPartyTool('expo-cli');
-  await useThirdPartyTool('expo', args, true);
+  await useThirdPartyTool('expo', args);
 };
--- src/fragments/create-project-from-expo.ts
+++ src/fragments/create-project-from-expo.ts
@@ -7,7 +7,8 @@ import { setPackageJsonValue, addScript } from '../actions/packageJson';
 import { jestConfigForReactNative } from '../actions/jest';
 
 export const appTs = `import App from './src/App';
-export default App;`
+export default App;
+`
 
 export const appTsx = `import * as React from 'react';
 import { Text, View } from 'react-native';
@@ -38,7 +39,7 @@ export const createProjectFromExpo = async () => {
   logger.context('Expo');
   logger.pending('initializing project (this might take a little while)');
   const cwd = process.cwd();
-  await expo('init --template blank --npm --workflow managed .');
+  await expo(`init --npm --template blank --workflow managed --non-interactive --name ${cwd.split('/').pop()} .`);
   process.chdir(cwd);
   logger.pending('tweaking project to use typescript');
   await rm('babel.config.js');
